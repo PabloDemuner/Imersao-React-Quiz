cd951279969e980c03518331797198a1adb34fb2
Pablo <pablodiegodemuner@gmail.com>
2021-01-29 22:17:12 -0400

    Implementacao dinamica da pagina e animacoes

--- quiz/pages/index.js
+++ quiz/pages/index.js
@@ -63,7 +63,23 @@ export default function Home() {
           <Widget.Content>
             <h1>Quizes da Galera</h1>
 
-            <p>lorem ipsum dolor sit amet...</p>
+            <ul>
+              {db.external.map((linkExterno) =>{
+                const [projectName, githubUser] = linkExterno
+                .replace(/\//g, ``)
+                .replace(`https:`, ``)
+                .replace(`.vercel.app`, ``)
+                .split(`.`);
+
+                return (
+                  <li>
+                    <Widget.Topic href={linkExterno}>
+                  {`${githubUser}/${projectName}`}
+                  </Widget.Topic>
+                  </li>
+                );
+              })}
+            </ul>
           </Widget.Content>
         </Widget>
         <Footer />
